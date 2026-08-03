4df2c5a6afbfd4cc0677f7b8f0635d07a839848d
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-08-03 20:47:42 +0300

    /* here's the big boy*/ now generates buttons with a loop - sets the unused buttons to empty - and has gaurdes against null and empty arrays

--- core/src/main/java/storyUtil/StoryDisplay.java
+++ core/src/main/java/storyUtil/StoryDisplay.java
@@ -13,17 +13,24 @@ import com.mygdx.game.Assets;
 
 import util.Util;
 
+// TODO make the GUI the way you imagined it	
 public class StoryDisplay {
+
+	public final static int buttonsCount = 3;
+
 	private int index = 3; // <- not a magic number - this is where the story starts
 
 	private Label dialogLabel;
 	private TextField takeInput;
-	
-	private TextButton option1;
-	private TextButton option2;
-	private TextButton option3;
+
+	private TextButton[] buttons = new TextButton[buttonsCount];
 
 	private void buildDialogUI(Stage stage) {
+
+		Table dialogsGUI = new Table();
+
+		dialogsGUI.setFillParent(true);
+		dialogsGUI.bottom();
 
 		dialogLabel = new Label("", Assets.skin);
 		dialogLabel.setWrap(true);
@@ -31,87 +38,78 @@ public class StoryDisplay {
 		takeInput = new TextField("", Assets.skin);
 		takeInput.setVisible(false);
 		takeInput.setPosition(250, 100, 10);
-		
-		option1 = new TextButton("", Assets.skin);
-		option1.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-			}
-		});
 
-		option2 = new TextButton("", Assets.skin);
-		option2.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-			}
-		});
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new TextButton("", Assets.skin);
+			buttons[i].addListener(new ClickListener() {
+				@Override
+				public void clicked(InputEvent event, float x, float y) {
+					index = (Integer) event.getListenerActor().getUserObject();
+					displayOptions();
+				}
+			});
 
-		option3 = new TextButton("", Assets.skin);
-		option3.addListener(new ClickListener() {
-			@Override
-			public void clicked(InputEvent event, float x, float y) {
-			}
-		});
+			dialogsGUI.add(buttons[i]).row();
 
-		Table dialogsGUI = new Table();
+		}
 
-		
-		dialogsGUI.setFillParent(true);
-		dialogsGUI.bottom();
 		dialogsGUI.add(dialogLabel).width(700).pad(10).row();
-		
-		dialogsGUI.add(option1).row();
-		dialogsGUI.add(option2).row();
-		dialogsGUI.add(option3).row();
 
 		stage.addActor(dialogsGUI);
 		stage.addActor(takeInput);
-		
 		stage.setDebugAll(true);
 	}
-	
-	
+
+	private int useages = 0;
 
 	public void launchStory(Stage stage) {
 		if (dialogLabel == null)
 			buildDialogUI(stage);
 
-		if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+		if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER) && useages < 2) {
+			useages++;
 			dialogLabel.setText(TextDecode.getText(index));
 			displayOptions();
 		}
 	}
-	private void displayOptions() {
-		
+
+	private boolean checkArray(int nextNodesLength) {
 		if (TextDecode.getNextNodes(index) == null) {
 			Util.log("end of nodes reached");
-			
-			return;
+
+			return true;
 		}
-		if (TextDecode.getNextNodes(index).length == 1) {
+		if (nextNodesLength == 0) {
+			Util.log("WARNING - empty array here on ID " + index);
+
+			return true;
+		}
+		return false;
+	}
+
+	private void displayOptions() {
+		int loops = 0;
+		int nextNodesLength = TextDecode.getNextNodes(index).length;
+
+		if (checkArray(nextNodesLength))
+			return;
+
+		// because the first node is always the other person's line this always happens
+		dialogLabel.setText(TextDecode.getText(TextDecode.getNextNodes(index)[0]));
+		if (nextNodesLength == 1) {
 			index = TextDecode.getNextNodes(index)[0];
+
 			return;
 		}
-		
-		
-		for (int i = 0; i < TextDecode.getNextNodes(index).length; i++) {
-			switch(i) {
-			case 0 -> {
-				dialogLabel.setText(TextDecode.getText(TextDecode.getNextNodes(index)[i]));
-			}
-			case 1 -> {
-				option1.setText(TextDecode.getText(TextDecode.getNextNodes(index)[i]));
-			}
-			case 2 -> {
-				option2.setText(TextDecode.getText(TextDecode.getNextNodes(index)[i]));
-			}
-			case 3 -> {
-				option3.setText(TextDecode.getText(TextDecode.getNextNodes(index)[i]));
-			}
-			default -> {
-				throw new IllegalStateException("the switch went into a case it didn't expect - chances are you added a 4th node");
-			}
-			}
+
+		for (int i = 1; i < nextNodesLength; i++) {
+			buttons[i - 1].setText(TextDecode.getText(TextDecode.getNextNodes(index)[i]));
+			buttons[i - 1].setUserObject(TextDecode.getNextNodes(index)[i]);
+			loops++;
+		}
+		for (int i = buttons.length - 1; i >= loops; i--) {
+			buttons[i].setText("");
+			buttons[i].setUserObject(null);
 		}
 	}
 }
