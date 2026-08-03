1c21712ff582f01c9cd7d6c8d567753e0e4f9876
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-08-03 20:46:07 +0300

    a new method "addToStory" makes it easier to add new story lines while also protecting against adding an array longer than the buttons i have

--- core/src/main/java/storyUtil/TextDecode.java
+++ core/src/main/java/storyUtil/TextDecode.java
@@ -9,60 +9,70 @@ import com.badlogic.gdx.utils.I18NBundle;
 
 public class TextDecode {
 
-	private TextDecode() {}
-	
+	private TextDecode() {
+	}
+
 	private static Map<Integer, DialogNode> story = new HashMap<>();
 	// TODO make this take the language the player selected
 	private static Locale locale = Locale.of("en");
 
-	private static I18NBundle bundle = I18NBundle.createBundle(
-		    Gdx.files.internal("translation/translation"), locale
-		);
-	
+	private static I18NBundle bundle = I18NBundle.createBundle(Gdx.files.internal("translation/translation"), locale);
+
 	static {
 		generateStory();
 	}
-	
-	private static void generateStory() {
-		story.put(1, (new DialogNode("retryButton")));
-		story.put(2, (new DialogNode("quitButton")));
-		story.put(3, (new DialogNode("hero.hey", new Integer[] {4})));
-		story.put(4, (new DialogNode("hero.askWhoYouAre", new Integer[] {4,5,6,7})));
-		story.put(5, (new DialogNode("player.imFriend")));
-		story.put(6, (new DialogNode("player.imEnemy")));
-		story.put(7, (new DialogNode("player.imhi")));
-		
+
+	private static void addToStory(Integer id, String line, Integer[] nodesTree) {
+		if (story.containsKey(id))
+			throw new IllegalStateException("the id :" + id + " already exists");
+		// if (nodesTree != null)
+		if (nodesTree.length - 1 > StoryDisplay.buttonsCount)
+			throw new IllegalArgumentException("the amount of nodes passed " + nodesTree.length
+					+ " are larger than the amounts of buttons we have");
+		story.put(id, (new DialogNode(line, nodesTree)));
 	}
-/**
- *  this is to decode the translations, see {@link Dialogue#generateStory()} for all IDs
- *  
- * @param id the ID of the line you want to show
- * @throws IllegalArgumentException if the ID you passed doesn't exist
- * @return String - the line you called
- */
-	public static String getText(int id) {
+
+	private static void generateStory() {
+		addToStory(1, "retryButton", null);
+		addToStory(2, "quitButton", null);
+		addToStory(3, "hero.hey", new Integer[] { 4 });
+		addToStory(4, "hero.askWhoYouAre", new Integer[] { 1, 2, 3, 4 });
+	}
+
+	/**
+	 * this is to decode the translations, see {@link Dialogue#generateStory()} for
+	 * all IDs
+	 * 
+	 * @param id the ID of the line you want to show
+	 * @throws IllegalArgumentException if the ID you passed doesn't exist
+	 * @return String - the line you called
+	 */
+	public static String getText(Integer id) {
 		DialogNode node = story.get(id);
-		if (node == null) throw new IllegalArgumentException("the id " + id + " doesn't exist");
-		
+		if (node == null)
+			throw new IllegalArgumentException("the id " + id + " doesn't exist");
+
 		// TEMP when testing re-enable the line below
 //	    Util.log("returned " + node.text + " here");
 		return bundle.get(node.text);
 	}
-	
+
 	/**
-	 *  this is to get each nodes next node ID, see {@link Dialogue#generateStory()} for all IDs
-	 *  
+	 * this is to get each nodes next node ID, see {@link Dialogue#generateStory()}
+	 * for all IDs
+	 * 
 	 * @param id the ID of the line you want to see the next line of
 	 * @throws IllegalArgumentException if the ID you passed doesn't exist
 	 * @return int - the ID of the line that links for the line you called
 	 */
 	public static Integer[] getNextNodes(int id) {
 		DialogNode node = story.get(id);
-		if (node == null) throw new IllegalArgumentException("the id " + id + " doesn't exist");
-		
+		if (node == null)
+			throw new IllegalArgumentException("the id " + id + " doesn't exist");
+
 		// TEMP when testing re-enable the line below
 //	    Util.log("returned " + node.nextNode + " here");
 		return node.nextNodes;
 	}
-	
+
 }
