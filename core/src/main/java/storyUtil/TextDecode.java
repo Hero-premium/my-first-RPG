6a1a4e6f4573ecac4406dbf19a66a477d59066ea
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-08-04 20:26:15 +0300

    re-enabled the null check in addToStory - getText now takes an Integer instead of an int for consistency - fixed old javadoc

--- core/src/main/java/storyUtil/TextDecode.java
+++ core/src/main/java/storyUtil/TextDecode.java
@@ -7,10 +7,14 @@ import java.util.Map;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.I18NBundle;
 
+import util.Util;
+
 public class TextDecode {
 
 	private TextDecode() {
 	}
+
+	private static int internalID = 1;
 
 	private static Map<Integer, DialogNode> story = new HashMap<>();
 	// TODO make this take the language the player selected
@@ -25,22 +29,31 @@ public class TextDecode {
 	private static void addToStory(Integer id, String line, Integer[] nodesTree) {
 		if (story.containsKey(id))
 			throw new IllegalStateException("the id :" + id + " already exists");
-		// if (nodesTree != null)
-		if (nodesTree.length - 1 > StoryDisplay.buttonsCount)
-			throw new IllegalArgumentException("the amount of nodes passed " + nodesTree.length
-					+ " are larger than the amounts of buttons we have");
+
+		if (nodesTree != null)
+			if (nodesTree.length - 1 > StoryDisplay.buttonsCount)
+				throw new IllegalArgumentException("the amount of nodes passed " + nodesTree.length
+						+ " are larger than the amounts of buttons we have");
+		if (id != internalID)
+			Util.log("WARNING - the ID passed " + id + " does not match the internal ID " + internalID
+					+ "make sure you did not make a mistake");
 		story.put(id, (new DialogNode(line, nodesTree)));
+		internalID++;
 	}
 
 	private static void generateStory() {
 		addToStory(1, "retryButton", null);
 		addToStory(2, "quitButton", null);
 		addToStory(3, "hero.hey", new Integer[] { 4 });
-		addToStory(4, "hero.askWhoYouAre", new Integer[] { 1, 2, 3, 4 });
+		addToStory(4, "hero.askWhoYouAre", new Integer[] { 1, 4, 3, 4 });
+		addToStory(5, "optionNull", null);
+		addToStory(6, "option0", new Integer[] {});
+		addToStory(7, "option1", new Integer[] { 6 });
+		addToStory(8, "option2", new Integer[] { 6, 5 });
 	}
 
 	/**
-	 * this is to decode the translations, see {@link Dialogue#generateStory()} for
+	 * this is to decode the translations, see {@link TextDecode#generateStory()} for
 	 * all IDs
 	 * 
 	 * @param id the ID of the line you want to show
@@ -58,14 +71,14 @@ public class TextDecode {
 	}
 
 	/**
-	 * this is to get each nodes next node ID, see {@link Dialogue#generateStory()}
+	 * this is to get each nodes next node ID, see {@link TextDecode#generateStory()}
 	 * for all IDs
 	 * 
 	 * @param id the ID of the line you want to see the next line of
 	 * @throws IllegalArgumentException if the ID you passed doesn't exist
-	 * @return int - the ID of the line that links for the line you called
+	 * @return Integer[] - the ID of the line that links for the lines you called
 	 */
-	public static Integer[] getNextNodes(int id) {
+	public static Integer[] getNextNodes(Integer id) {
 		DialogNode node = story.get(id);
 		if (node == null)
 			throw new IllegalArgumentException("the id " + id + " doesn't exist");
