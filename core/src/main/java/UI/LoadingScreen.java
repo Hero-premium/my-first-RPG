0ff44a7722c70b0d7482f9843e4cb9ed22b12f4a
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-08-28 15:24:03 +0300

    now draws the loading screen instead of printing to console (to be improved)

--- core/src/main/java/UI/LoadingScreen.java
+++ core/src/main/java/UI/LoadingScreen.java
@@ -1,26 +1,42 @@
 package UI;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.mygdx.game.Assets;
 import com.mygdx.game.GameLauncher;
 
-import util.Util;
-
 public class LoadingScreen extends ScreenAdapter {
 	private final GameLauncher game;
+	private SpriteBatch batch;
+	private BitmapFont font;
 
 	public LoadingScreen(GameLauncher game) {
 		this.game = game;
+		batch = new SpriteBatch();
+		font = new BitmapFont();
 	}
 
 	@Override
 	public void render(float delta) {
+		ScreenUtils.clear(0, 0, 0, 1);
+		batch.begin();
+		
 		if (!Assets.load()) {
-			Util.log("loading!" + (Assets.getProgress() * 100) + "%");
+			font.draw(batch, "loading!" + (Assets.getProgress() * 100) + "%", 50, 400);
+			batch.end();
 			return;
 		}
-		Util.log("finished");
-		game.setScreen(new MainGame());
+		
+		batch.end();
+		game.setScreen(new MainGame(game));
+		dispose();
 	}
 
+	@Override
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
 }
