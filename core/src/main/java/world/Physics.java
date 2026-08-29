ecf492c35d5d2c717fb32da035da145766587238
Hero-premium <288237143+Hero-premium@users.noreply.github.com>
2026-08-29 22:59:46 +0300

    collapsed the 2 almost identical methods into one

--- core/src/main/java/world/Physics.java
+++ core/src/main/java/world/Physics.java
@@ -6,75 +6,59 @@ import entities.Entity;
 
 public final class Physics {
 
-	private static final float GRAVITY = 980f;
-	private static final float AIR = 980f;
+    private static final float GRAVITY = 980f;
+    private static final float AIR = 980f;
 
-	private static void airRes(Entity entity, float deltaTime) {
-		if (entity.velocity.x == 0)
-			return;
+    private Physics() {
+        throw new AssertionError("No world.Physics instance for you!");
+    }
 
-		if (Math.abs(entity.velocity.x) < AIR * deltaTime) {
-			entity.velocity.x = 0;
-		} else {
-			float air = Physics.AIR * Math.signum(entity.velocity.x);
-			entity.velocity.x -= air * deltaTime;
-		}
+    private static float dampFloats(float velocity, float friction, float deltaTime) {
+        if (velocity == 0)
+            return 0;
 
-		entity.hitBox.x += entity.velocity.x * deltaTime;
-	}
-	
-	private static void flyablesGravity(Entity entity, float deltaTime) {
-		if (entity.velocity.y == 0)
-			return;
+        if (Math.abs(velocity) < friction * deltaTime) {
+            return 0;
+        }
+        float air = friction * Math.signum(velocity);
+        return velocity - air * deltaTime;
+    }
 
-		if (Math.abs(entity.velocity.y) < GRAVITY * deltaTime) {
-			entity.velocity.y = 0;
-		} else {
-			float gravity = Physics.GRAVITY * Math.signum(entity.velocity.y);
-			entity.velocity.y -= gravity * deltaTime;
-		}
+    /**
+     * applies airResistance and velocity clamping to all entities and applies
+     * gravity to all non-flying entities
+     *
+     * @param entities   the list containing all entities
+     * @param delta      deltaTime to prevent movement issues on higher FPS
+     * @param floorLevel the floor level this screen uses, no entity can go below it
+     *                   unless it can fly
+     */
+    public static void applyPhysics(Array<Entity> entities, float delta, float floorLevel) {
+        for (Entity object : entities) {
+            object.velocityClamp();
+            object.velocity.x = dampFloats(object.velocity.x, AIR, delta);
+            object.hitBox.x += object.velocity.x * delta;
 
-		entity.hitBox.y += entity.velocity.y * deltaTime;
-	}
+            if (!(object instanceof Flyable)) {
+                gravity(object, delta, floorLevel);
+            } else {
+                object.velocity.y = dampFloats(object.velocity.y, GRAVITY, delta);
+                object.hitBox.y += object.velocity.y * delta;
+            }
+        }
+    }
 
-	/**
-	 * applies airResistance and velocity clamping to all entities and applies
-	 * gravity to all non-flying entities
-	 * 
-	 * @param entities   the list containing all entities
-	 * @param delta      deltaTime to prevent movement issues on higher FPS
-	 * @param floorLevel the floor level this screen uses, no entity can go below it
-	 *                   unless it can fly
-	 */
-	public static void applyPhysics(Array<Entity> entities, float delta, float floorLevel) {
-		for (Entity object : entities) {
-			object.velocityClamp();
-			airRes(object, delta);
+    private static void gravity(Entity entity, float deltaTime, float floorLevel) {
 
-			if (!(object instanceof Flyable)) {
-				gravity(object, delta, floorLevel);
-			} else {
-				flyablesGravity(object, delta);
-				// TODO add flyable logic
-			}
-		}
-	}
+        entity.velocity.y -= GRAVITY * deltaTime;
 
-	private static void gravity(Entity entity, float deltaTime, float floorLevel) {
+        entity.hitBox.y += entity.velocity.y * deltaTime;
 
-		entity.velocity.y -= GRAVITY * deltaTime;
-
-		entity.hitBox.y += entity.velocity.y * deltaTime;
-
-		if (entity.hitBox.y < floorLevel) {
-			entity.hitBox.y = floorLevel;
-			entity.velocity.y = 0f;
-			entity.onGround = true;
-		}
-	}
-
-	private Physics() {
-		throw new AssertionError("No world.Physics instance for you!");
-	}
+        if (entity.hitBox.y < floorLevel) {
+            entity.hitBox.y = floorLevel;
+            entity.velocity.y = 0f;
+            entity.onGround = true;
+        }
+    }
 
 }
